79d9bf7302a05564fa191341b984c75dcaf4b38a
Reptop <raed.kabir03@gmail.com>
2025-03-13 19:41:08 -0700

    plz

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -3,35 +3,42 @@ import NavBar from '../navbar'
 import { Box, Container } from '@chakra-ui/react'
 import Footer from '../footer'
 
-
 const Main = ({ children, router }) => {
   return (
     <Box as="main" pb={8}>
       <Head>
+        {/* General Meta Tags */}
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Raed's homepage" />
+        <meta name="description" content="Raed Kabir's Portfolio Website" />
         <meta name="author" content="Raed Kabir" />
         <meta name="author" content="Reptop" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <meta name="twitter:title" content="Raed Kabir" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
-        <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
+
+        {/* Open Graph Meta Tags (for Discord, Facebook, etc.) */}
         <meta property="og:site_name" content="Raed Kabir" />
-        <meta name="og:title" content="Raed Kabir" />
+        <meta property="og:title" content="Raed Kabir | Portfolio" />
+        <meta property="og:description" content="Explore my portfolio! I'm a CS & Math student interested in networking, programming, and teaching." />
+        <meta property="og:image" content="https://reptop.dev/preview.png" />
+        <meta property="og:url" content="https://reptop.dev/" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://www.craftz.dog/card.png" />
-        <title>Raed Kabir - Homepage</title>
+
+        {/* Twitter Card Meta Tags (for better Twitter/X previews) */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Raed Kabir | Portfolio" />
+        <meta name="twitter:description" content="Explore my portfolio and projects!" />
+        <meta name="twitter:image" content="https://reptop.dev/preview.png" />
+        <meta name="twitter:site" content="@Reptop" />
+        <meta name="twitter:creator" content="@Reptop" />
+
+        {/* Page Title */}
+        <title>Raed Kabir - Portfolio</title>
       </Head>
 
       <NavBar path={router.asPath} />
 
       <Container maxW="container.md" pt={14}>
-
         {children}
-
         <Footer />
       </Container>
     </Box>
@@ -39,3 +46,4 @@ const Main = ({ children, router }) => {
 }
 
 export default Main
+
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,8 @@
-import Head from 'next/head'
 import Layout from '../components/layouts/main'
 import Fonts from '../components/fonts'
 import { AnimatePresence } from 'framer-motion'
 import Chakra from '../components/chakra'
-// import Payhip from '../components/payhip'
+import Payhip from '../components/payhip'
 import { Analytics } from '@vercel/analytics/react'
 
 if (typeof window !== 'undefined') {
@@ -13,22 +12,8 @@ if (typeof window !== 'undefined') {
 function Website({ Component, pageProps, router }) {
   return (
     <Chakra cookies={pageProps.cookies}>
-      <Head>
-        {/* Open Graph Meta Tags for Discord, Facebook, etc. */}
-        <meta property="og:title" content="Raed Kabir | Portfolio" />
-        <meta property="og:description" content="Check out my portfolio website! I am a CS & Math student passionate about networking, programming, and teaching." />
-        <meta property="og:image" content="https://reptop.dev/preview.png" />
-        <meta property="og:url" content="https://reptop.dev/" />
-        <meta property="og:type" content="website" />
-
-        {/* Twitter Meta Tags for better Twitter previews */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Raed Kabir | Portfolio" />
-        <meta name="twitter:description" content="Check out my portfolio website and projects!" />
-        <meta name="twitter:image" content="https://reptop.dev/preview.png" />
-      </Head>
-
       <Fonts />
+      <Payhip />
       <Layout router={router}>
         <AnimatePresence
           mode="wait"
@@ -48,4 +33,3 @@ function Website({ Component, pageProps, router }) {
 }
 
 export default Website
-
